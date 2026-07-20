21c65ff825fcb13ee02939447d27d59db76a95d9
Lucas Mattos <lucasmattos2209@gmail.com>
2026-07-20 19:50:31 -0300

    docs: aulas 169, 170 e 171

---
+++ aula171.py
@@ -0,0 +1,44 @@
+# reduce - faz a redução de um iterável em um valor
+
+from functools import reduce
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 60.90},
+]
+
+# def funcao_do_reduce(acumulador, produto):
+#     print('acumulador', acumulador)
+#     print('produto', produto)
+#     print()
+#     return acumulador +  produto['preco']
+
+# total = reduce(
+#     funcao_do_reduce,
+#     produtos,
+#     0
+# )
+
+total = reduce(
+    lambda ac, p: ac + p['preco'],
+    produtos,
+    0
+)
+
+print('Total é', total)
+
+# total = 0
+# for produto in produtos:
+#     total += produto['preco']
+
+# print(
+#     sum(
+#         [
+#             p['preco']
+#             for p in produtos
+#         ]
+#     )
+# )
